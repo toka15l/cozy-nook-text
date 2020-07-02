8560abeefa1ef6738946c6c7d0c5a72afc3adc74
Steven Lawson <visual.infusion@gmail.com>
2020-07-02 01:34:55 -0500

    - more organization and drawing of items

--- src/Item.hx
+++ src/Item.hx
@@ -4,6 +4,7 @@ import openfl.display.Bitmap;
 class Item
 {
 	public var bitmap:Bitmap = null;
+	public var needsRedraw:Bool = false;
 
 	public function new() {		
 	}
--- src/World.hx
+++ src/World.hx
@@ -1,6 +1,7 @@
 package;
 import openfl.display.Bitmap;
 import openfl.display.MovieClip;
+import openfl.geom.Point;
 
 class World extends MovieClip
 {
@@ -9,7 +10,7 @@ class World extends MovieClip
 	private var bitmaps:Bitmaps = null;
 	private var itemMatrix:Array<Array<Item>> = [];
 	
-	public function new() {		
+	public function new() {
 		super();
 		
 		scaleX = scaleY = INITIAL_SCALE;
@@ -20,9 +21,37 @@ class World extends MovieClip
 	}
 	
 	private function addItem(bitmap:Bitmap, x:Int, y:Int):Void {
-		bitmap.x = x * bitmaps.SPRITE_WIDTH;
-		bitmap.y = y * bitmaps.SPRITE_HEIGHT;
-		addChild(bitmap);
+		while (itemMatrix.length < x + 1) {
+			itemMatrix.push([]);
+		}
+		while (itemMatrix[x].length < y) {
+			itemMatrix[x].push(null);
+		}
+		var item:Item = new Item();
+		item.bitmap = bitmap;
+		item.needsRedraw = true;
+		itemMatrix[x][y] = item;		
+		redraw();
+	}
+	
+	private function redraw():Void {
+		for (x in 0...itemMatrix.length) {
+			for (y in 0...itemMatrix[x].length) {
+				var item:Item = itemMatrix[x][y];
+				if (item != null && item.needsRedraw == true) {
+					var removeArray:Array<Bitmap> = cast getObjectsUnderPoint(new Point((x * bitmaps.SPRITE_WIDTH) + (bitmaps.SPRITE_WIDTH / 2), (y * bitmaps.SPRITE_HEIGHT) + (bitmaps.SPRITE_HEIGHT / 2)));
+					for (remove in removeArray) {
+						removeChild(remove);
+					}
+					if (item.bitmap != null) {
+						item.bitmap.x = x * bitmaps.SPRITE_WIDTH;
+						item.bitmap.y = y * bitmaps.SPRITE_HEIGHT;
+						addChild(item.bitmap);
+						item.needsRedraw = false;
+					}
+				}
+			}
+		}
 	}
 	
 	public function zoomIn():Void {
