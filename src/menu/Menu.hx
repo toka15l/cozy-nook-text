02ebfb1a3111d66fec71914c38496583aa03a9eb
Steven Lawson <visual.infusion@gmail.com>
2021-01-11 23:42:35 -0600

    CNT-2 Menu enhancements - multiple select menu rendering - fix missing pixels for arrow piece

--- src/menu/Menu.hx
+++ src/menu/Menu.hx
@@ -49,16 +49,38 @@ class Menu extends Board
 	}
 	
 	public function addMultipleItemSelect(items:Array<WorldItem>, tileX:Int, tileY:Int):Void {
-		trace("multiple select: " + tileX + ", " + tileY);
-		var currentX:Int = tileX;
+		var menuSelectItems:Array<MenuSelectItem> = [];
 		for (item in items) {
 			var menuSelectItem:MenuSelectItem = new MenuSelectItem(item.spriteCharCode, item.color);
 			menuSelectItem.setBitmapData(spriteBitmapData.getBitmapDataForCharCode(menuSelectItem.spriteCharCode));
-			menuSelectItem.x = currentX * SpriteBitmapData.SPRITE_WIDTH;
-			menuSelectItem.y = (tileY + 1) * SpriteBitmapData.SPRITE_HEIGHT;
-			addChild(menuSelectItem);
-			currentX++;
+			menuSelectItems.push(menuSelectItem);
 		}
+		drawMenu(menuSelectItems, tileX, tileY);
+	}
+	
+	private function drawMenu(items:Array<MenuSelectItem>, tileX:Int, tileY:Int):Void {
+		drawBorderPiece(201, tileX - 1, tileY + 1); // top left
+		drawBorderPiece(186, tileX - 1, tileY + 2); // middle left
+		drawBorderPiece(200, tileX - 1, tileY + 3); // bottom left
+		for (i in 0...items.length) {
+			drawBorderPiece(i == 0 ? 207 : 205, tileX + i, tileY + 1); // arrow or top middle
+			items[i].x = (tileX + i) * SpriteBitmapData.SPRITE_WIDTH;
+			items[i].y = (tileY + 2) * SpriteBitmapData.SPRITE_HEIGHT;
+			addChild(items[i]);
+			drawBorderPiece(205, tileX + i, tileY + 3); // bottom middle
+		}
+		var rightX:Int = tileX + items.length;
+		drawBorderPiece(187, rightX, tileY + 1); // top right
+		drawBorderPiece(186, rightX, tileY + 2); // middle right
+		drawBorderPiece(188, rightX, tileY + 3); // bottom right
+	}
+	
+	private function drawBorderPiece(spriteCharCode:Int, tileX:Int, tileY:Int):Void {
+		var borderPiece:Item = new MenuItem(spriteCharCode);
+		borderPiece.setBitmapData(spriteBitmapData.getBitmapDataForCharCode(borderPiece.spriteCharCode));
+		borderPiece.x = tileX * SpriteBitmapData.SPRITE_WIDTH;
+		borderPiece.y = tileY * SpriteBitmapData.SPRITE_HEIGHT;
+		addChild(borderPiece);
 	}
 	
 	public function addMultipleActions(actions:Array<Action>, spriteCharCode:Int, color:Int, tileX:Int, tileY:Int):Void {
