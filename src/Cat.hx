5abb760242bd5e6beb2e48929ca60d69f2cd6aee
Steven Lawson <visual.infusion@gmail.com>
2021-01-26 21:29:26 -0600

    CNT-5 World - implement cat - refactor into moveTowardsDesiredCoordinates

--- src/Cat.hx
+++ src/Cat.hx
@@ -18,30 +18,25 @@ class Cat extends WorldItem
 		}));
 		
 		// cat walks
-		dispatchEvent(new WorldItemTickEvent(WorldItemTickEvent.REGISTER, this, 1, function () {
-			if (desiredX != null && desiredY != null && (desiredX != tileX || desiredY != tileY)) {
-				var movementX:Int = 0;
-				if (tileX > desiredX) {
-					movementX = -1;
-				} else if (tileX < desiredX) {
-					movementX = 1;
-				}
-				var movementY:Int = 0;
-				if (tileY > desiredY) {
-					movementY = -1;
-				} else if (tileY < desiredY) {
-					movementY = 1;
-				}
-				move(movementX, movementY);
+		dispatchEvent(new WorldItemTickEvent(WorldItemTickEvent.REGISTER, this, 1, moveTowardsDesiredCoordinates));
+	}
+	
+	private function moveTowardsDesiredCoordinates():Void {
+		if (desiredX != null && desiredY != null && (desiredX != tileX || desiredY != tileY)) {
+			var movementX:Int = 0;
+			if (tileX > desiredX) {
+				movementX = -1;
+			} else if (tileX < desiredX) {
+				movementX = 1;
 			}
-			
-			/*var tile:WorldTile = cast parent; // TODO: remove 'parent' usage - replace with event
-			var world:World = cast tile.parent;
-			var neighbors:Array<Array<WorldTile>> = world.getNeighbors(tile);
-			if (neighbors[2][1] == null) {
-				move(1, 0);
-			}*/
-		}));
+			var movementY:Int = 0;
+			if (tileY > desiredY) {
+				movementY = -1;
+			} else if (tileY < desiredY) {
+				movementY = 1;
+			}
+			move(movementX, movementY);
+		}
 	}
 }
 
